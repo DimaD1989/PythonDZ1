93afee8071cc7f0bc13ef5d81880b8e7695e4486
DimaD1989 <d_d1989sv@mail.ru>
2023-06-03 18:54:13 +0300

    task1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # Написать программу, которая будет выводить в консоль ёлочку заданной высоты.
 
+rows = int(input("Введите количество рядов в елке: "))
+count = 1
+step = " "
+step2 = 1
+star = "*"
+print(step)
+while (rows > 0):
+    fir_tree = step * (rows - count) + star * step2
+    print(fir_tree)
+    rows -=1
+    step2 +=2
+
