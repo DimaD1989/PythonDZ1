e60707c0303dd22847625dd3eef0741679723850
DimaD1989 <d_d1989sv@mail.ru>
2023-06-05 19:09:26 +0300

    end

--- main.py
+++ main.py
@@ -70,3 +70,25 @@
 
 # Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток. Программа должна подсказывать «больше» или «меньше» после каждой попытки.
 
+from random import randint
+
+LIMIT_MIN = 0
+LIMIT_MAX = 1000
+COUNT_TRY = 9
+RAND_NUMBER = randint(LIMIT_MIN,LIMIT_MAX)
+
+is_win = True
+
+for _ in range(COUNT_TRY + 1):
+    number = int(input("Введите число от 0 до 1000: "))
+    if number > RAND_NUMBER:
+        print("Ваше число БОЛЬШЕ загаданного")
+        is_win = False
+    elif number < RAND_NUMBER:
+        print("Ваше число МЕНЬШЕ загаданного")
+        is_win = False
+    else:
+        print('Вы выиграли!')
+        is_win = True
+if not is_win:
+    print(" Вы проиграли")
